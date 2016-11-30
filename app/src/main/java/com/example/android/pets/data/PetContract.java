d43c303bac637cd6288287fd97718ff366503d55
Todd Skinner <todd.skinner@gmail.com>
2016-11-30 15:11:54 -0800

    Add dbHelper

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,27 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by toddskinner on 11/30/16.
+ */
+
+public final class PetContract {
+    public static abstract class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pet.
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+    }
+}
