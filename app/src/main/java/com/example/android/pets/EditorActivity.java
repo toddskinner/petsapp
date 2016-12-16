16b35d0ccfe77b69e8edb255672dadb97cccd10f
Todd Skinner <todd.skinner@gmail.com>
2016-12-15 22:22:15 -0800

    Fix hide delete menu option when insert new item

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -91,6 +91,9 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         if (mCurrentPetUri == null) {
             //this is a new pet, so change the app bar to say "Add a Pet"
             setTitle(getString(R.string.editor_activity_title_new_pet));
+
+            //invalidate the options menu, so the "Delete" menu option can be hidden
+            //it doesn't make sense to delete a pet that hasn't been created yet
         } else {
             //Otherwise this is an existing pet, so change app bar to say "Edit Pet"
             setTitle(R.string.editor_activity_title_edit_pet);
@@ -194,6 +197,17 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new pet, hide the "Delete" menu item.
+        if (mCurrentPetUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
         return true;
     }
 
